6517bc0207b98e48af53a7746f88cfd3cf220e10
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-06-05 19:35:13 -0300

    feat: cancelar publicação da função de soma

--- math.go
+++ math.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Soma(10, 10))
+	fmt.Println(soma(10, 10))
 }
 
 // Soma returns the sum of two integers.
-func Soma(a int, b int) int {
+func soma(a int, b int) int {
 	return a + b
 }
